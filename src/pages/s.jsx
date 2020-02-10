c9c52411d5161efe502b4a2182d196b5bf87a0fd
Shreyas Minocha <sm-git@riseup.net>
2020-02-10 19:45:13 +0530

    Change redirect method

--- src/pages/s.jsx
+++ src/pages/s.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import {Redirect} from '@reach/router';
+import React from 'react';
 
 class Page extends React.Component {
     constructor(props) {
@@ -13,18 +12,26 @@ class Page extends React.Component {
         const apiUrl = `${protocol}//${host}/api/s/${id}`;
 
         console.log(apiUrl);
+        let to = '';
+
+        try {
+            const response = await fetch(apiUrl);
+            to = await response.text();
+        } catch (error) {
+            console.error(error);
+        }
 
         this.setState({
-            url: await (await fetch(apiUrl)).text()
+            url: to
         });
     }
 
     render() {
-        if (this.state.url !== null) {
-            return <Redirect to={`${this.state.url}`}/>;
+        if (this.state.url && typeof window !== `undefined`) {
+            window.location.replace(this.state.url);
         }
 
-        return <div></div>;
+        return <></>;
     }
 }
 
