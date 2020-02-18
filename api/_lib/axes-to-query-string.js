193d18844bce0c09c060b0d75aaf8bd0f7726a6a
Shreyas Minocha <sm-git@riseup.net>
2020-02-18 14:37:19 +0530

    Fix axesToQueryString for undefined, null

--- api/_lib/axes-to-query-string.js
+++ api/_lib/axes-to-query-string.js
@@ -1,6 +1,10 @@
 const qs = require('query-string');
 
 function axesToQueryString(axes) {
+    if (axes === undefined || axes === null) {
+        axes = {};
+    }
+
     const axesArray = Object.entries(axes);
     const modifiedAxesArray = axesArray.filter(([key, val]) => val !== '0');
     const modifiedAxes = Object.fromEntries(modifiedAxesArray);
